58ed06ab921829790c45dc13b568c9e422dc9e08
Apostolos Delis <apostolos.delis@yahoo.com>
2018-03-04 14:12:04 -0800

    SQL DataBase Generator

--- create_ddl.py
+++ create_ddl.py
@@ -1,6 +1,6 @@
-from app.yahoo_answers_data_scrapping.post import Post
-from app.yahoo_answers_data_scrapping.user import User
-from app.yahoo_answers_data_scrapping.comment import Comment
+from post import Post
+from user import User
+from comment import Comment
 
 """Create the 3 tables within the Yahoo Answers Database"""""
 User.create_table()
